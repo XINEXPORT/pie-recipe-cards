d3658bb8de38c10c3e8e852386ec23adfcbb37f9
Christine Hoang <christinehoangt@gmail.com>
2023-11-28 09:22:58 -0600

    delete card function is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import pieData from './pieData.js'
 import { useState } from 'react';
 
+
 function PieCard(props){
 
+
 const [showPicture, setShowPicture] = useState(true);
-let {name, temp, ingredients, imgUrl, cardId} = props
+let {name, temp, ingredients, imgUrl, cardId, deleteCard} = props
+
+function handleCard(){
+  deleteCard(cardId)
+}
 
 function toggleCard(){
   setShowPicture(!showPicture);
 }
+
 
 if(showPicture){
   return (
     <div className="card" onClick={toggleCard}>
     <h2>{props.name}</h2>
     <img src={props.imgUrl}></img>
+    <button onClick={handleCard} style={{display: 'flex'}}>X</button>
     </div>
-  );} else{
-
+  );} else {
   const ingredientsDisplay=[];
   for(let item in ingredients){
     ingredientsDisplay.push (<p> {item}: {ingredients[item]}</p>)
   }
-
   return(
     <div className="card" onClick={toggleCard} style={{background: '#ffd8db'}}>
     <h2>{props.name}</h2>
@@ -30,10 +36,16 @@ if(showPicture){
     {ingredientsDisplay}
     </div>
   )
-}}
+  }}
 
 function App(){
-  const cards = pieData.map(({name, temp, ingredients, imgUrl, cardId })=>
+  const [pieDataArr, setPieData]=useState(pieData);
+  function deleteCard(cardId){
+    const updatePieData = pieDataArr.filter((card)=> card.cardId !== cardId);
+    console.log(updatePieData);
+    setPieData(updatePieData);}
+
+  const cards = pieDataArr.map(({name, temp, ingredients, imgUrl, cardId })=>
   <PieCard
   name = {name}
   temp = {temp}
@@ -41,6 +53,7 @@ function App(){
   imgUrl = {imgUrl}
   cardId = {cardId}
   key={cardId}
+  deleteCard={deleteCard}
   />);
 return<>{cards}</>
 }
