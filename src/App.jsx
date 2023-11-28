36ca2a8f87dcd65b10d989f28153c8a2eb830070
Christine Hoang <christinehoangt@gmail.com>
2023-11-27 22:09:22 -0600

    initsave

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import pieData from './pieData.js'
+import { useState } from 'react';
+
+function PieCard(props){
+
+const [showPicture, setShowPicture] = useState(true);
+let {name, temp, ingredients, imgUrl, cardId} = props
+
+function toggleCard(){
+  setShowPicture(!showPicture);
+}
+
+if(showPicture){
+  return (
+    <div className="card" onClick={toggleCard}>
+    <h2>{props.name}</h2>
+    <img src={props.imgUrl}></img>
+    </div>
+  );} else{
+
+  const ingredientsDisplay=[];
+  for(let item in ingredients){
+    ingredientsDisplay.push (<p> {item}: {ingredients[item]}</p>)
+  }
+
+  return(
+    <div className="card" onClick={toggleCard}>
+    <h2>{props.name}</h2>
+    <p>{props.temp}</p>
+    {ingredientsDisplay}
+    </div>
+  )
+}}
+
+function App(){
+  const cards = pieData.map(({name, temp, ingredients, imgUrl, cardId })=>
+  <PieCard
+  name = {name}
+  temp = {temp}
+  ingredients = {ingredients}
+  imgUrl = {imgUrl}
+  cardId = {cardId}
+  key={cardId}
+  />);
+return<>{cards}</>
+}
+
+export default App;
---
+++ src/pieData.js
@@ -0,0 +1,60 @@
+const pieData = [
+    {
+      name: 'Chocolate Pie',
+      temp: 'Bake at 350F',
+      ingredients: {'Bittersweet Chocolate': '6.5oz', 'Sugar': '1.5 Cups', 'Milk': '3 Cups', 'Butter': '2 Tbs'},
+      imgUrl: '../public/img/chocolate-pie.jpg',
+      cardId: 1,
+    },
+    {
+      name: 'Apple Pie',
+      temp: 'Bake at 340F',
+      ingredients: { 'Green Apples': '5 Cups', 'Sugar': '3 Tbs', 'Flour': '1.5 Cups', 'Butter': '1 Cup', 'Cinnamon': '2 Tsp' },
+      imgUrl: '../public/img/apple-pie.PNG',
+      cardId: 2,
+    },
+    {
+      name: 'Cherry Pie',
+      temp: 'Bake at 360F',
+      ingredients: { 'Cherries': '1 Cup', 'Sugar': '5 Tbs', 'Cornstarch': '4 Tbs', 'Sugar': '1 Cup' },
+      imgUrl: '../public/img/cherry-pie.jpg',
+      cardId: 3,
+    },
+    {
+      name: 'Peach Pie',
+      temp: 'Bake at 340F',
+      ingredients: { 'Yellow Peaches': '2 Cups', 'Sugar': '1 Cup', 'Butter': '5 Tbs', 'Nutmeg': '1 Tsp'},
+      imgUrl: '../public/img/peach-pie.jpg',
+      cardId: 4,
+    },
+    {
+      name: 'Pecan Pie',
+      temp: 'Bake at 380F',
+      ingredients: { 'Pecans': '2 Cups', 'Sugar':'0.5 Cup', 'Molasses': '0.5 Cup', 'Butter': '8 Tbs'},
+      imgUrl: '../public/img/pecan-pie.PNG',
+      cardId: 5,
+    },
+    {
+      name: 'Pumpkin Pie',
+      temp: 'Bake at 350F',
+      ingredients: { 'Pumpkin Puree': '1 Can', 'Graham Crackers': '2 Cups', 'Pumpkin Spice': '16 Tbs', 'Honey': '1 Cup'},
+      imgUrl: '../public/img/pumpkin-pie.PNG',
+      cardId: 6,
+    },
+    {
+      name: 'Blueberry Pie',
+      temp: 'Bake at 360F',
+      ingredients: { 'Fresh Blueberries': '2 Cups', 'Sugar': '1 Cup', 'Cornstarch': '2 Tbs', 'Flour': '1 Cup'},
+      imgUrl: '../public/img/blueberry-pie.PNG',
+      cardId: 7,
+    },
+    {
+      name: 'Raisin',
+      temp: 'Bake at 350F',
+      ingredients: { 'Raisins': '2 Cups', 'Jamaican Rum': '1 Cup', 'Sour Cream': '1.5 Cups', 'Vanilla': '2 Tbs'},
+      imgUrl: '../public/img/raisin-pie.PNG',
+      cardId: 8,
+    },
+  ];
+
+  export default pieData;
